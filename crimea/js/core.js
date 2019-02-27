c0e31721f7622c89b86c96347d0901ab92bf45dc
mkubibaev <mkubibaev@gmail.com>
2019-02-27 17:47:52 +0600

    crimea link scroll

--- crimea/js/core.js
+++ crimea/js/core.js
@@ -2,14 +2,35 @@ $(document).ready(function(){
 
 // $(".map__marker").not(":first").hide();
 
-$(".map__list li").click(function() {
-	$(".map__list li").removeClass("active").eq($(this).index()).addClass("active");
-	$(".map__marker").removeClass("active").eq($(this).index()).addClass("active");
-}).eq(0).addClass("active");
+	$(".map__list li").mouseover(function() {
+		$(".map__list li").removeClass("active").eq($(this).index()).addClass("active");
+		$(".map__marker").removeClass("active").eq($(this).index()).addClass("active");
+	}).eq(0).addClass("active");
 
-$(".map__marker").click(function() {
-	$(".map__list li").removeClass("active").eq($(this).index()).addClass("active");
-	$(".map__marker").removeClass("active").eq($(this).index()).addClass("active");
-}).eq(0).addClass("active");
+	$(".map__marker").mouseover(function() {
+		$(".map__list li").removeClass("active").eq($(this).index()).addClass("active");
+		$(".map__marker").removeClass("active").eq($(this).index()).addClass("active");
+	}).eq(0).addClass("active");
+
+
+	$(".map__list li a").click(function(e) {
+		e.preventDefault();
+		
+		var position = $($(this).attr("href")).offset().top;
+
+		$("body, html").animate({
+			scrollTop: position
+		}, 500 );
+	});
+
+		$(".map__markers a").click(function(e) {
+		e.preventDefault();
+		
+		var position = $($(this).attr("href")).offset().top;
+
+		$("body, html").animate({
+			scrollTop: position
+		}, 500 );
+	});
 
 })
