e27c115e585c1c5caea889d1db084a2c9f13fc27
mkubibaev <mkubibaev@gmail.com>
2019-02-27 17:03:15 +0600

    crimea markers

--- crimea/js/core.js
+++ crimea/js/core.js
@@ -1,66 +1,15 @@
 $(document).ready(function(){
 
-//main-nav btn
-  $('#menu-btn').click(function(){ 
-    $('.main-nav__list').slideToggle(700);
-    if ($(this).hasClass('not-active')) {
-      $(this).addClass('is-active').removeClass('not-active');
-    }else{
-      setTimeout(function(){
-      $('.is-active').addClass('not-active').removeClass('is-active')
-      },600)   
-    }     
-  });
+// $(".map__marker").not(":first").hide();
 
-//slick sliders initial
-	$('.main-slider').slick({
-		slidesToShow: 1,
-		slidesToScroll: 1,
-	  autoplay: true,
-	  autoplaySpeed: 3000,		
-		arrows: false,
-		dots: true,
-    responsive: [
-      {
-      breakpoint: 990,
-        settings: {
-          arrows: false,
-          }
-        }
-      ]    
-	});
+$(".map__list li").click(function() {
+	$(".map__list li").removeClass("active").eq($(this).index()).addClass("active");
+	$(".map__marker").removeClass("active").eq($(this).index()).addClass("active");
+}).eq(0).addClass("active");
 
-$('.partners__slider').slick({
-    slidesToShow: 5,
-    slidesToScroll: 1,
-    arrows: true,
-    dots: false,
-    responsive: [
-    {
-      breakpoint: 1200,
-        settings: {
-          slidesToShow: 4,
-        }
-      },
-    {
-      breakpoint: 992,
-        settings: {
-          slidesToShow: 3,
-        }
-      },
-     {
-      breakpoint: 768,
-        settings: {
-          slidesToShow: 2,
-        }
-      },
-     {
-      breakpoint: 576,
-        settings: {
-          slidesToShow: 1,
-        }
-      }     
-      ]         
-  });
+$(".map__marker").click(function() {
+	$(".map__list li").removeClass("active").eq($(this).index()).addClass("active");
+	$(".map__marker").removeClass("active").eq($(this).index()).addClass("active");
+}).eq(0).addClass("active");
 
 })
