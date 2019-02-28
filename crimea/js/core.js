7adddad5ae74aaa7cedf198330cd587f9c8002ba
mkubibaev <mkubibaev@gmail.com>
2019-02-28 14:37:01 +0600

    scroll to top

--- crimea/js/core.js
+++ crimea/js/core.js
@@ -1,15 +1,17 @@
 $(document).ready(function(){
 
-// $(".map__marker").not(":first").hide();
+	$(".map__title").not(":first").hide();
 
 	$(".map__list li").mouseover(function() {
 		$(".map__list li").removeClass("active").eq($(this).index()).addClass("active");
 		$(".map__marker").removeClass("active").eq($(this).index()).addClass("active");
+		$(".map__title").hide().eq($(this).index()).fadeIn();
 	}).eq(0).addClass("active");
 
 	$(".map__marker").mouseover(function() {
 		$(".map__list li").removeClass("active").eq($(this).index()).addClass("active");
 		$(".map__marker").removeClass("active").eq($(this).index()).addClass("active");
+		$(".map__title").hide().eq($(this).index()).fadeIn();
 	}).eq(0).addClass("active");
 
 
@@ -31,6 +33,20 @@ $(document).ready(function(){
 		$("body, html").animate({
 			scrollTop: position
 		}, 500 );
+	});
+
+
+	$(window).scroll(function(){
+		if ($(window).scrollTop() >= 600) {
+			$('#scrollTop').show();
+		} else {
+			$('#scrollTop').hide();
+		}
+	});
+
+	$("#scrollTop").click(function() {
+		$("html, body").animate({ scrollTop: 0 }, "slow");
+		return false;
 	});
 
 })
